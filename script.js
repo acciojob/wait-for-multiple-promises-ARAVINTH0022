e6ad6ab1c8d5f0180eee2e502f965854bb9f9398
ARAVINTH0022 <113502178+ARAVINTH0022@users.noreply.github.com>
2026-08-10 22:59:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+const outputBody = document.getElementById('output');
+outputBody.innerHTML = `<tr id="loading"><td colspan="2">Loading...</td></tr>`;
+
 function createPromise(id) {
   const time = Math.random() * 2 + 1; // Random time between 1 and 3 seconds
   return new Promise((resolve) => {
@@ -7,26 +10,24 @@ function createPromise(id) {
   });
 }
 
-const startTime = performance.now();
-
 const p1 = createPromise(1);
 const p2 = createPromise(2);
 const p3 = createPromise(3);
 
 Promise.all([p1, p2, p3]).then((results) => {
-  const endTime = performance.now();
-  const totalTime = (endTime - startTime) / 1000;
-
-  const outputBody = document.getElementById('output');
   outputBody.innerHTML = '';
 
+  let maxTime = 0;
   results.forEach((res) => {
+    if (res.time > maxTime) {
+      maxTime = res.time;
+    }
     const row = document.createElement('tr');
     row.innerHTML = `<td>Promise ${res.id}</td><td>${res.time.toFixed(3)}</td>`;
     outputBody.appendChild(row);
   });
 
   const totalRow = document.createElement('tr');
-  totalRow.innerHTML = `<td>Total</td><td>${totalTime.toFixed(3)}</td>`;
+  totalRow.innerHTML = `<td>Total</td><td>${maxTime.toFixed(3)}</td>`;
   outputBody.appendChild(totalRow);
 });
